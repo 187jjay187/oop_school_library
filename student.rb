6e576bfe0a825aea17cb93b2873d4635106f8c34
187jjay187 <187jjay187@gmail.com>
2023-02-08 17:50:01 +0200

    Add student.rb class

---
+++ student.rb
@@ -0,0 +1,13 @@
+# student.rb
+class Student < Person
+  def initialize(name: "Unknown", age:, parent_permission: true, classroom:)
+    super(name: name, age: age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  attr_reader :classroom
+
+  def play_hooky
+    "¯\\(ツ)/¯"
+  end
+end
