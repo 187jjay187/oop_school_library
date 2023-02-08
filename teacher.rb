3ea399ac6f78d741bdda4d012a6ab10634de0f67
187jjay187 <187jjay187@gmail.com>
2023-02-08 17:50:33 +0200

    Add teacher.rb class

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+# teacher.rb
+class Teacher < Person
+  def initialize(name: "Unknown", age:, parent_permission: true, specialization:)
+    super(name: name, age: age, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  attr_reader :specialization
+
+  def can_use_services?
+    true
+  end
+end
