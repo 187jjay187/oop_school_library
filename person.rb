e38c60f9eab47bed9e8febd15edf11e937f50852
187jjay187 <187jjay187@gmail.com>
2023-02-08 17:49:23 +0200

    Add person class

---
+++ person.rb
@@ -0,0 +1,26 @@
+# person.rb
+class Person
+  attr_reader :id, :name, :age
+  attr_writer :name, :age
+
+  @@counter = 0
+
+  def initialize(name: "Unknown", age:, parent_permission: true)
+    @id = @@counter += 1
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def is_of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+end
